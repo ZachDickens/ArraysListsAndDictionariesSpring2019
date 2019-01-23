8bd952fce0074c2f987e86d9f493a9663cbf8cc4
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-01-23 15:44:33 -0500

    First Commit

---
+++ ArraysListsAndDictionariesSpring2019/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysListsAndDictionariesSpring2019
+{
+    public class Car
+    {
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int MilesPerGallon { get; set; }
+    }
+}
---
+++ ArraysListsAndDictionariesSpring2019/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysListsAndDictionariesSpring2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fordFiesta = new Car("Ford", "Fiesta", 2013);
+            var subaruLegacy = new Car("Subaru", "Legacy", 2017);
+            var hondaCrv = new Car("Honda", "CRV", 2014);
+
+            var parkedCars = new Car[5];
+            Console.WriteLine(parkedCars.Length);
+
+            parkedCars[0] = hondaCrv;
+
+            var firstCar = parkedCars[0];
+            Console.WriteLine(firstCar.Make);
+
+            var luxuryCars = new List<Car>();
+            
+            var foo = new List<string>();
+            var bar = new List<int>();
+
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
